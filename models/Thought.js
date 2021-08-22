ab87a81bd7aec07aebe2e86866d11011aea7bd86
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-08-22 00:57:59 -0400

    reaction schema created

--- models/Thought.js
+++ models/Thought.js
@@ -1,11 +1,42 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require("dateformat");
 
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment's _id field
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal, "dddd, mmmm dS, yyyy, h:MM:ss TT")
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
             type: String,
             required: true,
+            minLength: 1,
+            maxLength: 280
         },
         createdAt: {
             type: Date,
@@ -27,7 +58,7 @@ const ThoughtSchema = new Schema(
     }
 );
 
-// get total count of friends
+// get total count of reactions
 ThoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
