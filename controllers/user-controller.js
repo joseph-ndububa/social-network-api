b9538f48901e34fdd56721b74271dbe7ab751037
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-08-22 01:18:42 -0400

    post user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -33,4 +33,11 @@ const userController = {
             });
     },
 
+    // post a user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
 }
