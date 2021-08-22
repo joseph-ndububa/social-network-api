bb8b74e00207ea51187e66366f8a3f3e547b42ba
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-08-22 00:11:01 -0400

    server created

---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
